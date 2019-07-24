ce107998b74897c2cc0ffe231b9a291525a29b9e
PhysPeach <35629670+PhysPeach@users.noreply.github.com>
2019-07-24 15:53:40 +0900

    ネットから引っ張ってきた。

--- MesureTime.cpp
+++ MesureTime.cpp
@@ -1,17 +1,61 @@
 #include<iostream>
 #include<chrono>
 
+#define N 10
+
+class Matrix {
+private:
+	int dim;
+public:
+	double** Mat;
+
+	Matrix(int n) {
+		dim = n;
+
+		Mat = new double*[dim];
+
+		for (int i = 0; i < dim; i++) {
+			Mat[i] = new double[dim];
+		}
+		for (int i = 0; i < dim; i++){
+			for (int j = 0; j < dim; j++) {
+				Mat[i][j] = (double)(i * dim + j);
+			}
+		}
+	}
+	~Matrix() {
+		delete[] Mat;
+		for (int i = 0; i < dim; i++) {
+			delete[] Mat[i];
+		}
+	}
+	void maltiple(Matrix a, Matrix b) {
+		for (int i = 0; i < dim; i++) {
+			for (int j = 0; j < dim; j++) {
+				for (int k = 0; k < dim; k++) {
+					Mat[i][j] = a.Mat[i][k] + b.Mat[k][j];
+				}
+			}
+		}
+	}
+};
+
 int main()
-{
+{	
+	Matrix A(N);
+	Matrix B(N);
+	Matrix C(N);
 
 	std::chrono::system_clock::time_point start, end;
+	std::cout << "start" << std::endl;
 	start = std::chrono::system_clock::now();
 
-	/*
-	ˆ—
-	*/
-	end = std::chrono::system_clock::now();
+	//Routine
+	for (int i = 0; i < 1000; i++) {
+		C.maltiple(A, B);
+	}
 
+	end = std::chrono::system_clock::now();
 	auto elapsed = std::chrono::duration_cast<std::chrono::milliseconds>(end - start).count();
 	std::cout << elapsed << "ms" << std::endl;
 	return 0;
